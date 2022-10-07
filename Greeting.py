52444c8e29219a1417894e2c1f5ad22fa9a89014
amirghari <99399497+amirghari@users.noreply.github.com>
2022-10-07 14:08:04 +0300

    1st Draft

--- Greeting.py
+++ Greeting.py
@@ -9,14 +9,31 @@ connection = mariadb.connect(
          )
 print("Hello! Welcome to Flight Game")
 
+# player_name = input("Enter Your Name")
+#
+# def greetings(name):
+#     sql = "INSERT INTO game (screen_name, co2_consumed, co2_budget, location) VALUES ('"+name+"', 0, 10000,'EFHK');"
+#     cursor = connection.cursor()
+#     cursor.execute(sql)
+#     connection.commit()
+#     return
+# greetings(player_name)
+
+
 player_name = input("Enter Your Name")
 
 def greetings(name):
-    sql = "SELECT * FROM game INSERT INTO game (screen_name, co2_budget) VALUES('"+name+"');"
+    max_id = "SELECT MAX(id) from game;"
+    cursor = connection.cursor()
+    cursor.execute(max_id)
+    # connection.commit()
+    max_num = int(cursor.fetchall()[0][0])
+    sql = "INSERT INTO game (id, screen_name, co2_consumed, co2_budget, location) VALUES(" + str(max_num + 1) +",'"+name+"', 0, 10000,'EFHK');"
     cursor = connection.cursor()
     cursor.execute(sql)
-    result = cursor.fetchall()
-    return result
+    cursor.fetchall()
+    connection.commit()
+    return
 greetings(player_name)
 
 
--- Location_Function.py
+++ Location_Function.py
@@ -18,8 +18,32 @@ def get_municipality(id):
     return
 
 airport_id = input("ENTER ident")
-get_municipality()
+get_municipality(airport_id)
+
+
+
+
+player_name = input("Enter Your Name")
+
+def greetings(name):
+    max_id = "SELECT MAX(id) from game;"
+    cursor = connection.cursor()
+    cursor.execute(max_id)
+    # connection.commit()
+    max_num = int(cursor.fetchall()[0][0])
+    sql = "INSERT INTO game (id, screen_name, co2_consumed, co2_budget, location) VALUES(" + str(max_num + 1) +",'"+name+"', 0, 10000,'EFHK');"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    cursor.fetchall()
+    connection.commit()
+    return
+greetings(player_name)
 
 
 # The input is Ident(primary key of the airport table)
 # The output is the name of the airport and municipality.
+
+
+
+
+
