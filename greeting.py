16baa44f0abd6ec55d47170a3d2e8a8d1082621e
Arina Vasileva <107575324+arinavasileva@users.noreply.github.com>
2022-10-10 13:23:00 +0300

    1st draft

---
+++ greeting.py
@@ -0,0 +1,9 @@
+name = input("Enter your name:")
+print('Hello name ! Welcome to Flight Game! Please select one of the following options:')
+def options_function():
+    print("View current location")
+    print("View goals")
+    print("View co2 budget")
+    print("Travel to a new airport")
+    return
+options_function()
