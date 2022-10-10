a7b57fc70f2cbc39257e6aae123b3b76602ee698
Arina Vasileva <107575324+arinavasileva@users.noreply.github.com>
2022-10-10 13:26:04 +0300

    Update options.py

--- options.py
+++ options.py
@@ -11,13 +11,13 @@ def options_function():
 
 
 options_selection = int(input("Enter the number of the selected function:"))
-if options_selection = 1 :
+if options_selection == 1 :
     current_icao(id)
-if options_selection = 2:
+if options_selection == 2:
     game_goals(id)
-if options_selection = 3:
+if options_selection == 3:
     available_co2(id)
-elif options_selection = 4:
+elif options_selection == 4:
 
     else:
     print("There is not such function number")
