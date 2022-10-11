76509ed91933ca56dd422d12f4b1e929a85d7918
amerilain <amerilain@gmail.com>
2022-10-11 10:41:35 +0300

    working main. needs functionality to view goals for option 2

--- main.py
+++ main.py
@@ -36,9 +36,7 @@ def get_municipality(icao):
     cursor = connection.cursor()
     cursor.execute(location)
     result = cursor.fetchall()
-    for row in result:
-        print(f"You are located in {row[0]} in {row[1]}.")
-    return
+    return result
 
 def latitude_and_longitude(icao):
     sql = "SELECT latitude_deg, longitude_deg FROM airport WHERE ident= '" + icao + "';"
@@ -69,8 +67,12 @@ def travel(id, icao):
     return
 
 
-def update_co2_budget(co2_left, trip_distance, id):
-    new_co2 = str(co2_left - trip_distance)
+def update_co2_budget(trip_distance, id):
+    sql = "SELECT co2_consumed FROM game WHERE id ='" + id + "';"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    new_co2 = str(int(result[0][0]) + trip_distance)
     sql = "UPDATE game SET co2_consumed=" + new_co2 + " WHERE id='" + id + "';"
     cursor = connection.cursor()
     cursor.execute(sql)
@@ -78,7 +80,7 @@ def update_co2_budget(co2_left, trip_distance, id):
 
 def random_weather():
     temperature = random.randint(-40, 40)
-    conditions = random.choice(['Cloudy', 'Clear'])
+    conditions = random.choice(['Clouds', 'Clear'])
     wind = random.randint(0, 15)
     return temperature, conditions, wind
 
@@ -158,10 +160,24 @@ def goals_achieved(temperature, conditions, wind):
 
     return achieved_goals
 
+def check_goals(id):
+    sql = "SELECT goal_id FROM goal_reached WHERE game_id = '" + str(id) + "'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    response = cursor.fetchall()
+    return response
 
 # if weather conditions meet any goals update goals_reached table
 def update_goals_reached(goals_to_update, id):
-    for i in goals_to_update:
+    list1 = []
+    # check existing goals to compare with new goals to avoid duplicates
+    for i in check_goals(id):
+        list1.append(i[0])
+    list2 = goals_to_update
+    # create a list of goals achieved that are not already in table:
+    new_list = [item for item in list2 if item not in list1]
+    # insert new and unique goals into table
+    for i in new_list:
         goal = str(i)
         sql = "INSERT INTO goal_reached (goal_id, game_id) VALUES ('" + goal + "', '" + id + "');"
         cursor = connection.cursor()
@@ -179,7 +195,7 @@ def count_goals(id):
 # main:
 # When a player starts the game, they are greeted and asked to enter their name.
 print(f"Welcome to FLight game!")
-player_name = str(input("Enter Your Name to start a new game:"))
+player_name = str(input("Enter Your Name to start a new game: "))
 
 # Their name is saved to the game table of our flight_game database and they are given a c02 budget of 10000.
 greetings(player_name)
@@ -194,68 +210,57 @@ while available_co2(player_name) > 0:
     print("2- view goals.")
     print("3- view co2 budget.")
     print("4- travel to new airport")
+
     menu_input = int(input("Please Enter the number of the command which you want to run: "))
+
+    # If the player selects 'view current location': - The player's current location is displayed on the screen
     if menu_input == 1:
-        get_municipality(current_icao(player_id))
+        airport = get_municipality(current_icao(player_id))
+        print(f"You are located in {airport[0][0]} in {airport[0][1]}.")
+
+    # If the player selects 'view goals': - A list of remaining goals appear
     if menu_input == 2:
-        print("Print goals achieved")
+        print(f"you have achieved goals {check_goals(player_id)}")
+
+    # If the player selects 'view co2 budget': - The remaining co2 in the player's budget is displayed
     elif menu_input == 3:
         availableCo2 = available_co2(player_name)
         print(f"Your available CO2 is {availableCo2}.")
+
+    # If the player selects 'travel to a new airport'
     elif menu_input == 4:
-        icao = input("Enter the ICAO code of your destination.")
-        lat_and_long = latitude_and_longitude(icao)
+        icao = input("Enter the ICAO code of your destination: ")
+        # calculate distance to destination
         distance = calculate_distance_km(icao, current_icao(player_id))
+        # calculate available co2
         availableCo2 = available_co2(player_name)
+        # ensure player has sufficient co2 in budget for trip
         if distance < availableCo2:
+            # update player location
             travel(player_id,icao)
-            update_co2_budget(availableCo2, distance, player_id)
+            # update player co2 budget
+            update_co2_budget(distance, player_id)
+            # generate weather conditions
             weather = random_weather()
+            # create list of goals that the weather has achieved for the player
             goals = goals_achieved(weather[0], weather[1], weather[2])
+            # use the goals list to update the goals reached table
             update_goals_reached(goals, player_id)
-            print(weather)
-            print(goals)
+            # inform player of game progress
+            print(f"Welcome to {get_municipality(current_icao(player_id))}!")
+            print(f"Temperature: {weather[0]}C")
+            print(f"Conditions: {weather[1]}")
+            print(f"Wind: {weather[2]}m/s")
+            print(f"You have achieved goals {goals}!")
+            # Every round, check if the player has achieved enough goals to win the game
             if count_goals(player_id) >= 5:
                 print("You win!")
                 break
 
-
         else:
-            print("you don't have C02 budget.")
+            print("you don't have C02. Select a closer location")
     else:
-        continue
+        print("choose only a number between 1 and 4")
 
-
-
-
-
-
-
-# - view current location
-# - view goals (need function)
-# - view co2 budget
-# - travel to new airport
-
-
-# -> If the player selects 'view current location': - The player's current location is displayed on the screen
-
-# -> If the player selects 'view goals': - A list of remaining goals appear
-
-# -> If the player selects 'view co2 budget': - The remaining co2 in the player's budget is displayed
-
-# -> If the player selects 'travel to a new airport'
-
-# if the player has enough c02>0 : - The player is asked to enter an ICAO code for the airport the wish to tavel to:
-
-# After the player enters the code, the program checks that the player has enough co2 budgeted for the trip and informs them how far away the airport is and how much co2 will be consumed. If they have enough co2, they are then asked if they want to proceed. If not, they return to the first options list.
-
-   # - If the player selects yes:
-   #   - the players location is updated
-   #   - the players co2 budgeted is updated
-   #   - goals acheived are updated if any weather conditions correnspond with unrealized goals
-   #   - All updated information is displayed to the player.
-   #   - The player then goes back to the first option list
-   #   - Check if the goals that are acheived are >=5 ; if so
-   #   - player WINS!
 
 print("Game Over")
